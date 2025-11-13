76eb1f2810ae96f45ab778a7133aa03ec48f422b
Lexus <mamrenko.alexey@gmail.com>
2025-11-13 12:00:19 +0100

    Commit changes with .env

--- src/main/java/project/ToDoList/ToDoListApplication.java
+++ src/main/java/project/ToDoList/ToDoListApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ToDoListApplication {
 
 	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(ToDoListApplication.class, args);
 	}
 
